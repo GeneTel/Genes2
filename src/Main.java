b9b6865c53a9c182eba724dae746d347a4a738a3
GeneTel <skycrewllc@gmail.com>
2023-01-29 22:10:57 -0500

    HW2

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,39 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println(sumOFDigits(sumOFDigits(233)));
+        sumOfCharacters("Hi", "Gene");
+        sumOfCharacters("Hi", "Gene", "Telepov");
+        compareNumber(5);
+    }
 
-        int num = 225;
-        int rem = 0;
-        int sum = 0;
-        while (num > 0) {
-            rem = num % 10;
-            sum = sum + rem;
-            num = num / 10;
-            System.out.println(sum);
+
+    static int sumOFDigits(int i) {
+        int sumOfNumbers = i % 10 + (i / 10) % 10 + i / 100;
+        return sumOfNumbers;
+    }
+
+    static void sumOfCharacters(String word1, String word2) {
+        String concatStrings = word1 + word2;
+        int lengthOfTwoWords = concatStrings.length();
+        System.out.println("The sum of characters for provided 2 words: " + lengthOfTwoWords);
+    }
+
+    static void sumOfCharacters(String word1, String word2, String word3) {
+        String concatStrings = word1 + word2 + word3;
+        int lengthOfThreeWords = concatStrings.length();
+        System.out.println("The sum of characters for provided 3 words: " + lengthOfThreeWords);
+    }
+
+    static boolean compareNumber(int number) {
+        if (number > 0) {
+            System.out.println("Your number is larger than 0");
+            return true;
+        } else {
+            System.out.println("Your number is not larger than 0");
+            return false;
         }
+
 
     }
 }
